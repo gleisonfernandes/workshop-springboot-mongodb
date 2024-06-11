6a0fb78ef2fbea84a89cfa93a29714865d318f6d
Gleison Fernandes da Silva <gleison.fernandesb@gmail.com>
2024-06-11 16:42:14 -0300

    :sparkles: feat: User insertion with post

--- src/main/java/com/gfsoftware/workshopmongo/services/UserService.java
+++ src/main/java/com/gfsoftware/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gfsoftware.workshopmongo.domain.User;
+import com.gfsoftware.workshopmongo.dto.UserDTO;
 import com.gfsoftware.workshopmongo.repository.UserRepository;
 import com.gfsoftware.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail()); 
 	}
 }
