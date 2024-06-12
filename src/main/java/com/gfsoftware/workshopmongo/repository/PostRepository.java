6c3e9d930b979aa3b7fe261ea3cbd56fbee99649
Gleison Fernandes da Silva <gleison.fernandesb@gmail.com>
2024-06-12 11:26:57 -0300

    :sparkles: feat: Creating Post entity with nested User

---
+++ src/main/java/com/gfsoftware/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.gfsoftware.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gfsoftware.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
