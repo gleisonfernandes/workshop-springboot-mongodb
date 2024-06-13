18f10d531fa823a91f7d1a7d4f94206f15283024
Gleison Fernandes da Silva <gleison.fernandesb@gmail.com>
2024-06-13 12:44:28 -0300

    :sparkles: feat: Adding comments to posts

--- src/main/java/com/gfsoftware/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gfsoftware/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.gfsoftware.workshopmongo.domain.Post;
 import com.gfsoftware.workshopmongo.domain.User;
 import com.gfsoftware.workshopmongo.dto.AuthorDTO;
+import com.gfsoftware.workshopmongo.dto.CommentDTO;
 import com.gfsoftware.workshopmongo.repository.PostRepository;
 import com.gfsoftware.workshopmongo.repository.UserRepository;
 
@@ -45,6 +46,15 @@ public class Instantiation implements CommandLineRunner {
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
 
